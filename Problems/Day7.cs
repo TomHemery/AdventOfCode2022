51101d84b0274eabf46114ac9808fe72d6a9636c
Tom Hemery <tomhemery@outlook.com>
2022-12-07 10:49:23 +0000

    Day7

---
+++ Problems/Day7.cs
@@ -0,0 +1,58 @@
+namespace AdventOfCode2022
+{
+    class Day7 : Problem
+    {
+
+        protected List<long> dirSizes = new();
+
+
+        public Day7(string inputPath) : base(inputPath)
+        {
+        }
+
+
+        protected int calculateDirectorySize(ref int curr)
+        {
+            int dirSize = 0;
+            do {
+                if (!puzzleInputLines[curr].StartsWith("dir") && !puzzleInputLines[curr].StartsWith("$")) {
+                    dirSize += int.Parse(puzzleInputLines[curr].Split(" ")[0]);
+                    curr ++; // To next line
+                } else if (puzzleInputLines[curr].StartsWith("$ cd") && !puzzleInputLines[curr].Contains("..")) {
+                    curr ++; // Step over cd into dir
+                    dirSize += calculateDirectorySize(ref curr);
+                } else {
+                    curr ++; // To next line
+                }
+            } while (curr < puzzleInputLines.Length && !puzzleInputLines[curr].StartsWith("$ cd .."));
+            curr ++; // Step over cd ..
+            dirSizes.Add(dirSize);
+            return dirSize;
+        }
+
+        public override string Part1()
+        {
+            dirSizes.Clear();
+            int start = 1; // skip the cd /
+            calculateDirectorySize(ref start);
+            int totalSize = 0;
+            foreach (int dirSize in dirSizes.Select(x => x > 100000 ? 0 : x)) {
+                totalSize += dirSize;
+            }
+            return totalSize.ToString();
+        }
+
+        public override string Part2()
+        {
+            dirSizes.Clear();
+            int start = 1; // skip the cd /
+            int fileSystemSize = calculateDirectorySize(ref start);
+
+            int spaceToDelete = 30000000 - (70000000 - fileSystemSize);
+            var largeDirs = dirSizes.Where(x => x >= spaceToDelete).ToList();
+            largeDirs.Sort();
+            
+            return largeDirs[0].ToString();
+        }
+    }
+}
